1b9abafed09ce8da18ef773aa13b0149b0dc16ef
md <md@2d70b15c-62de-0310-b45c-a2b26695ab02>
2005-07-23 06:24:28 +0000

    aufraeumen vor der abreise
    git-svn-id: http://svn.23.nu/svn/repos/DeNiSe/trunk@717 2d70b15c-62de-0310-b45c-a2b26695ab02

--- DeNiSePkg.py
+++ DeNiSePkg.py
@@ -10,9 +10,11 @@ encodeDns() or decodeDns() methods.
 Data structure:
 
 TTFFSLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+TFSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 T = Packet Type
 F = Flags
+C - ClientID
 S = Sequence number
 L = Length of Payload
 X = Payload (max 74 bytes)
@@ -66,52 +68,71 @@ class Encoder:
       self._sequence_number = 0
       self.domains = domains
       self.cipher = Crypto.Cipher.AES.new(pad(key, 16))
+      self.maxfragmentsize = 72
 
    def encode64(self, inbuf):       
       return base64.encodestring(inbuf).replace('+', '-').replace('/', '_').replace('\n', '').strip()
 
-   def encodePacket(self, ptype, flags, data):
-      s = "%s%s%s%s%s%s%s" % (chr((ptype >> 8) & 0xff),
-                              chr(ptype & 0xff),
-                              chr((flags >> 8) & 0xff),
-                              chr(flags & 0xff),
-                              chr(self._sequence_number),
-                              chr(len(data)), data)
-      self._sequence_number = (self._sequence_number + 1) % 256
+   def encodePacket(self, data, flags, seq):
+      """Encode a single Packet of Data"""
+      
+      s = "%s%s%s%s" % (chr(flags),
+                        chr(seq),
+                        chr(len(data)),
+                        data)
       s = padRand(s, 16)
       s = self.cipher.encrypt(s)
       s = self.encode64(s)
       s = s.replace('=', '')
       return s
 
-   def encodeData(self, ptype, flags, data):
+   def fragmentData(self, data, flags=0):
+      """Break down a big chunk of bytes into packets and encode them."""
       self._sequence_number = 0
       cdata = zlib.compress(data)
       ret = []
-      for i in range(0, len(cdata), 74):
-         if i > len(cdata) - 74:
+      for i in range(0, len(cdata), self.maxfragmentsize):
+         if i > len(cdata) - self.maxfragmentsize:
             # last packet
-            ret.append(self.encodePacket(ptype, flags | FLAG_LASTFRAGMENT, cdata[i:i+74]))
+            ret.append(self.encodePacket(cdata[i:i+self.maxfragmentsize],
+                                         flags | FLAG_LASTFRAGMENT,
+                                         self._sequence_number))
          else:
-            ret.append(self.encodePacket(ptype, flags, cdata[i:i+74]))
+            ret.append(self.encodePacket(cdata[i:i+self.maxfragmentsize],
+                                         flags, self._sequence_number,
+                                         self._sequence_number))
+         self._sequence_number = (self._sequence_number + 1) % 256
       return ret
 
-   def encodeDns(self, ptype, flags, data):
+   def encodeDns(self, data, flags=0):
+      """Break down a big chunk of bytes into packets and encode them as dmonainnames"""
       ret = []
-      for x in self.encodeData(ptype, flags, data):
+      for x in self.fragmentData(data, flags):
          # split into host and subdomain
          spos = random.randrange(max([len(x) - 62, 0]), min([63, len(x)]))
          ret.append("x%s.%s" % ('.x'.join([x[:spos], x[spos:]]), random.choice(self.domains)))
       return ret
 
-   def encodeDnsQuery(self, ptype, flags, data):
+   def encodeDnsQuery(self, data, flags=0):
       ret = []
-      for x in self.encodeDns(ptype, flags, data):      
+      for x in self.encodeDns(data, flags):      
          m = DNS.Lib.Mpacker()
          m.addHeader(1234, 0, DNS.Opcode.QUERY, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0)
          m.addQuestion(x, DNS.Type.TXT, DNS.Class.IN)
          ret.append(m.getbuf())
       return ret
+
+   def encodeDnsResponse(self, data, query='\0x5dummy\0x7example\0x3com\0x0', flags=0,):
+      ret = []
+      for x in self.fragmentData(data, flags):      
+         m = DNS.Lib.Mpacker()
+         m.addHeader(1234, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0)
+         m.addQuestion('\0x3bla\0x0', DNS.Type.TXT, DNS.Class.IN)
+         m.addTXT(query, DNS.Class.IN, 3, x)
+         ret.append(m.getbuf())
+      return ret
+
+
 
 class Decoder:
    def __init__(self, key, domains):
@@ -119,6 +140,7 @@ class Decoder:
       self.domains = domains
       self.datastore = {}
       self.seen_lastfragment = 1
+      self.packetqueue = []
 
    def checkDataComplete(self):
       k = self.datastore.keys()
@@ -132,7 +154,7 @@ class Decoder:
       k.sort()
       ret = []
       for i in k:
-         ret.append(self.datastore[i][3])
+         ret.append(self.datastore[i][0])
       return zlib.decompress(''.join(ret))
       
    def removeDomain(self, data):
@@ -141,6 +163,7 @@ class Decoder:
             return data[:data.find(x)]
 
    def rfc1035ToFqdn(self, data):
+      """convert an "pascal encoded" dns name in an packetet to a string"""
       ret = []
       i = 0
       c = ord(data[i])
@@ -159,22 +182,21 @@ class Decoder:
       s = pad(inbuf, 4)
       s = self.decode64(s + '=')
       s = self.cipher.decrypt(s)
-      ptype = (ord(s[0]) << 8) + ord(s[1])
-      flags = (ord(s[2]) << 8) + ord(s[3])
-      seq = ord(s[4])
-      plen = ord(s[5])
-      data = s[6:plen + 6]
-      return (ptype, flags, seq, data)
+      flags = ord(s[0])
+      seq = ord(s[1])
+      plen = ord(s[2])
+      data = s[3:plen + 3]
+      return (data, flags, seq)
 
    def decodeData(self, data):
       for p in data:
-         ptype, flags, serial, payload = self.decodePacket(p)
-         self.datastore[serial] = (ptype, flags, serial, payload)
+         payload, flags, serial = self.decodePacket(p)
+         self.datastore[serial] = (payload, flags, serial)
          if flags & FLAG_LASTFRAGMENT == FLAG_LASTFRAGMENT:
             self.seen_lastfragment = 1
       if self.seen_lastfragment == 1:
          if self.checkDataComplete():
-            return ptype, flags, serial, self.packetsToData()
+            return self.packetsToData(), (flags ^ FLAG_LASTFRAGMENT)
       return None
 
    def decodeDns(self, data):
@@ -184,98 +206,24 @@ class Decoder:
          buf.append(self.removeDomain(s.replace('.x', '')))
       return self.decodeData(buf)
 
+   def decodeDnsQuery(self, datal):
+      self.packetqueue.extend(datal) 
+      if not self.packetqueue:
+         return None
+      data = self.packetqueue.pop(0)
+      while data:
+         s = data[12:]
+         s = self.rfc1035ToFqdn(s)
+         ret = self.decodeDns([s])
+         if ret:
+            return ret
+         if not self.packetqueue:
+            return None
+         data = self.packetqueue.pop(0)
 
-   def decodeDnsQuery(self, data):
-      "feed me one packet at time"
-      s = data[12:]
-      s = self.rfc1035ToFqdn(s)
-      return self.decodeDns([s])
    
-class BEncoder:
-   
-   def __init__(self, key):
-      self._sequence_number = 0
-      self.cipher = Crypto.Cipher.AES.new(pad(key, 16))
-
-
-   def encodePacket(self, ptype, flags, data):
-      s = "%s%s%s%s%s%s%s" % (chr((ptype >> 8) & 0xff),
-                              chr(ptype & 0xff),
-                              chr((flags >> 8) & 0xff),
-                              chr(flags & 0xff),
-                              chr(self._sequence_number),
-                              chr(len(data)), data)
-      self._sequence_number = (self._sequence_number + 1) % 256
-      s = padRand(s, 16)
-      s = self.cipher.encrypt(s)
-      return s
-
-   def encodeData(self, ptype, flags, data):
-      self._sequence_number = 0
-      cdata = zlib.compress(data)
-      ret = []
-      for i in range(0, len(cdata), 350):
-         if i > len(cdata) - 350:
-            # last packet
-            ret.append(self.encodePacket(ptype, flags | FLAG_LASTFRAGMENT, cdata[i:i+350]))
-         else:
-            ret.append(self.encodePacket(ptype, flags, cdata[i:i+350]))
-      return ret
-
-   def encodeDnsResponse(self, query, ptype, flags, data):
-      ret = []
-      for x in self.encodeData(ptype, flags, data):      
-         m = DNS.Lib.Mpacker()
-         m.addHeader(1234, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0)
-         m.addQuestion('\0x3bla\0x0', DNS.Type.TXT, DNS.Class.IN)
-         m.addTXT(query, DNS.Class.IN, 3, x)
-         ret.append(m.getbuf())
-      return ret
    
 class BDecoder:
-   def __init__(self, key):
-      self.cipher = Crypto.Cipher.AES.new(pad(key, 16))
-      self.datastore = {}
-      self.seen_lastfragment = 1
-
-   def checkDataComplete(self):
-      k = self.datastore.keys()
-      for i in range(max(k)):
-         if not  self.datastore.has_key(i+1):
-            return None
-      return 1
-
-   def packetsToData(self):
-      k = self.datastore.keys()
-      k.sort()
-      ret = []
-      for i in k:
-         ret.append(self.datastore[i][3])
-      return zlib.decompress(''.join(ret))
-      
-   
-   def decodePacket(self, inbuf):
-      s = self.cipher.decrypt(inbuf)
-      ptype = (ord(s[0]) << 8) + ord(s[1])
-      flags = (ord(s[2]) << 8) + ord(s[3])
-      seq = ord(s[4])
-      plen = ord(s[5])
-      data = s[6:plen + 6]
-      return (ptype, flags, seq, data)
-
-   def decodeData(self, data):
-      for p in data:
-         ptype, flags, serial, payload = self.decodePacket(p)
-         self.datastore[serial] = (ptype, flags, serial, payload)
-         if flags & FLAG_LASTFRAGMENT == FLAG_LASTFRAGMENT:
-            self.seen_lastfragment = 1
-      if self.seen_lastfragment == 1:
-         if self.checkDataComplete():
-            return ptype, flags, serial, self.packetsToData()
-      return None
-
-   def decodeDns(self, data):
-      return self.decodeData(data)
 
 
    def decodeDnsQuery(self, data):
@@ -286,10 +234,17 @@ class BDecoder:
       for x in  r.answers:
          if x['typename'] == 'TXT':
             return self.decodeDns(x['data'])
-   
+
+def test(indata, transform1, transform2):
+   print indata,
+   x = transform1(*indata)
+   print x,
+   y = transform2(*(x,))
+   print y, transform1, transform2
+   assert y == indata
 
 if __name__ == '__main__':
-   t = """Dies ist ein Test Text.
+   longdata = """Dies ist ein Test Text.
 
    Und der sollte schön lang sein, damit er kompremiert immernoch
    deutlich länger als 74 Zeichen ist und in mehr als ein Datenpaket
@@ -299,13 +254,15 @@ if __name__ == '__main__':
    
    e = Encoder('key', ['t.23.nu', 't.bewaff.net', 't.c0re.jp', 't.lolitacoders.org'])
    d = Decoder('key', ['t.23.nu', 't.bewaff.net', 't.c0re.jp', 't.lolitacoders.org'])
+
+
+
+   for data in ("Test", longdata):
+      data = "Test"
+      test((data, 0, 0), e.encodePacket, d.decodePacket)
+      test((data, 0), e.encodeDns, d.decodeDns)
+      test((data, 0), e.encodeDnsQuery, d.decodeDnsQuery)
+      print e.encodeDnsResponse("TEST")
    #x = e.encodePacket(0,0, "Test")
-   #x = e.encodeData(0, 0, t)
-   #print d.decodeDns(e.encodeDns(0, 0, t))
-   #print d.decodeData(x)
-   #print d.decodeDnsQuery('\x00\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\rx8k5Wycuh7X-G xmo9hAtQMyGpvIs6WJy79cO-u_Mx-Wyg\x01t\x04c0re\x02jp\x00\x00\x10\x00\x01')
-   e = BEncoder('key')
-   d = BDecoder('key')
-   x = e.encodePacket(0,0, "Test")
-   x = e.encodeDnsResponse('\0x3die\0x8original\0x5query\0x5kommt\0x4hier\x02de\0x0', 0, 0, "Test")
-   print d.decodeDnsQuery(x)
+   #x = e.encodeDnsResponse('\0x3die\0x8original\0x5query\0x5kommt\0x4hier\x02de\0x0', 0, 0, "Test")
+   #print d.decodeDnsQuery(x)
