eee34f4eb7096ed9b948a09110a6bb4e28c78720
md <md@2d70b15c-62de-0310-b45c-a2b26695ab02>
2006-01-22 16:48:12 +0000

    git-svn-id: http://svn.23.nu/svn/repos/DeNiSe/trunk@817 2d70b15c-62de-0310-b45c-a2b26695ab02

--- massquery.py
+++ massquery.py
@@ -4,16 +4,20 @@ import socket, time, sys, random
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # s.setblocking(0)
 
-name = 'mara-%06i.test.23.nu'
-ip = '213.221.113.2'
-for i  in range (100000):
+name = 'pdns-%06i.test.23.nu'
+ip = '127.0.0.1'
+
+bytes = 0
+
+for i  in range (15000):
   d = DNS()
   q = DNSQR()
-  q.qname = name % i
+  q.qname = (name) % i
   d.rd = 1
   d.qd = q
   d.do_build()
   p = str(d)
-  bytes += s.sendto(p, 0, (ip, 53))
+  bytes += s.sendto(p, 0, (ip, 5353))
+  print q.qname
   time.sleep(0.01)
     
